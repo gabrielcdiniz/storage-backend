1b32ff544a070369360ed014f85d7275ec6121f0
Gabriel Diniz <gabriel.correa@twsolutions.com.br>
2021-05-23 13:11:47 -0300

    add: config module at project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+
+config();
 
 async function bootstrap() {
+  const {
+    SERVER_PORT,
+  } = process.env;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(SERVER_PORT);
 }
 bootstrap();
